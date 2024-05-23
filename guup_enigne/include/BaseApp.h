c816388db81776959642e8ca1686b87eb45694eb
Enrique Orihuela <eparis@atlassian.com>
2024-05-23 10:51:38 -0600

    window implementation
    create the code for the window

---
+++ guup_enigne/include/BaseApp.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "PreRequisitos.h"
+#include "Window.h"
+class BaseApp
+{
+public:
+	BaseApp() = default;
+	~BaseApp() = default;
+
+	HRESULT init();
+
+	void update();
+
+	void render();
+
+	void destroy();
+
+	int run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow);
+
+	LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+private:
+	window m_window;
+};
---
+++ guup_enigne/include/PreRequisitos.h
@@ -0,0 +1,12 @@
+#pragma once
+//librerias STD
+#include <windows.h>
+#include <xnamath.h>
+#include <string>
+//Librerias DirectX
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include "resource.h"
+//Third Parties
+

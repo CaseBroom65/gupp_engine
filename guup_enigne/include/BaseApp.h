cbeb6bd6ea05006d9b6ea966f65bde0775988f98
Enrique Orihuela <eparis@atlassian.com>
2024-08-06 10:52:59 -0600

    implementation core abstraction

--- guup_enigne/include/BaseApp.h
+++ guup_enigne/include/BaseApp.h
@@ -1,6 +1,23 @@
 #pragma once
 #include "PreRequisitos.h"
 #include "Window.h"
+#include "Device.h"
+#include "DeviceContext.h"
+#include "swap chain.h"
+#include "Texture.h"
+#include "depthstencilView.h"
+#include "RenderTargetView.h"
+#include "ViewPort.h"
+#include "ShaderProgram.h"
+#include "Buffer.h"
+#include "SampleState.h"
+#include "ModelLoader.h"
+
+#include "UserInterface.h"
+#include "Actor.h"
+
+
+
 // Define una clase llamada BaseApp.
 class BaseApp
 {
@@ -28,11 +45,49 @@ public:
     // - HINSTANCE hPrevInstance: Manejador de instancia previa (obsoleto y siempre NULL en aplicaciones modernas).
     // - LPWSTR lpCmdLine: Línea de comandos.
     // - int nCmdShow: Parámetro que especifica cómo debe mostrarse la ventana.
-    int run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow);
+    int run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow, WNDPROC wndproc);
 
-    // Declaración del método WndProc que es el procedimiento de ventana.
-    LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+    void CreateGrid(int width, int depth, float spacing);
+
 private:
     // Instancia de la clase Window.
-    window m_window;
+    window                                  m_window;
+    Device									m_device;
+    DeviceContext							m_deviceContext;
+    SwapChain								m_swapchain;
+    Texture									m_BackBuffer;
+    Texture									m_DepthStencil;
+    DepthStencilView						m_depthStencilView;
+    RenderTargetView						m_RenderTargetView;
+    Viewport								m_viewport;
+    ShaderProgram							m_shaderProgram;
+    std::vector<Buffer>						m_vertexBuffers;
+    std::vector<Buffer>						m_indexBuffers;
+    Buffer									m_CBBufferNeverChanges;
+    Buffer									m_CBBufferChangeOnResize;
+    Buffer									m_CBBufferChangesEveryFrame;
+
+    //Mesh									m_mesh;
+    std::vector<Texture>					modelTextures;
+    SamplerState							m_sampler;
+    ModelLoader								m_model;
+    Texture									m_default;
+    std::vector<Texture>					gridTexs;
+    UserInterface                            m_UserInterface;
+
+    //XMMATRIX                            m_World;
+    XMMATRIX                            m_View;
+    XMMATRIX                            m_Projection;
+    XMFLOAT4                            m_vMeshColor;
+    CBNeverChanges                      cbNeverChanges;
+    CBChangeOnResize                    cbChangesOnResize;
+    CBChangesEveryFrame                 cb;
+    //Grid Data
+    MeshComponent                       MC;
+    EngineUtilities::TSharedPointer<Actor> AGrid;
+
+    //Vela Actor
+    EngineUtilities::TSharedPointer<Actor> AVela;
+    //std::vector<Texture> gridTexs;
+
 };
